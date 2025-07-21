6479121b4d32a4265de206d8684a3d0fc0709d31
aminekcmi <aminekacimi25@gmail.com>
2025-07-21 15:42:22 +0100

    Add convex and clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexReactClient } from "convex/react";
 
 export const metadata: Metadata = {
   title: "The Word Escape",
   description: "The Word Escape game",
 };
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function RootLayout({
   children,
@@ -14,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+          <ConvexProviderWithClerk client={convex} useAuth={useAuth}>{children}</ConvexProviderWithClerk>
+        </ClerkProvider>
       </body>
     </html>
   );
