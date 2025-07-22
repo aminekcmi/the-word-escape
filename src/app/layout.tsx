309033db0a358a754100ed563947bb2016a893f8
aminekcmi <aminekacimi25@gmail.com>
2025-07-22 09:01:28 +0100

    Remove unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ConvexReactClient } from "convex/react";
 import ConvexClientProvider from "@/providers/ConvexClientProvider";
 import { ClerkProvider } from "@clerk/nextjs";
 
@@ -8,8 +7,6 @@ export const metadata: Metadata = {
   title: "The Word Escape",
   description: "The Word Escape game",
 };
-
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function RootLayout({
   children,
